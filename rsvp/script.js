b2b9d4b957e6f59583977205d6a8c326d599bf7f
Phil <github@exclsr.com>
2012-09-02 01:01:16 -0700

    RSVP: Add service to share scope data

--- rsvp/script.js
+++ rsvp/script.js
@@ -1,26 +1,35 @@
-angular.module('project',[]). 
-  config(function($routeProvider) {
-    $routeProvider.
-      when('/', {controller:Controller, templateUrl:'rsvp.html'}).
-      otherwise({redirectTo:'/'});
-  });
- 
- function Controller($scope) {
-  // Defaults
-  $scope.master= {
-    dancer : { 
-      role : "mystery"      
-    },
-    payment : "never"
-  };
- 
-  $scope.update = function(person) {
-    $scope.master= angular.copy(person);
-  };
- 
-  $scope.reset = function() {
-    $scope.person = angular.copy($scope.master);
-  };
- 
-  $scope.reset();  
+var projectModule = angular.module('project',[]);
+
+projectModule.config(function($routeProvider) {
+	$routeProvider.
+	when('/', {controller:PersonCtrl, templateUrl:'start.html'}).
+	when('/payment', {controller:PaymentCtrl, templateUrl:'payment.html'}).
+	otherwise({redirectTo:'/'});
+});
+
+projectModule.factory('personService', function() {  
+	return {
+		person : {
+			dancer : {
+				role : "mystery"
+			},
+			payment : "never"
+		}
+	};
+});
+
+function PersonCtrl($scope, $location, personService) {
+
+	$scope.person = personService.person;
+
+	$scope.toPayment = function() {        
+		personService.person = $scope.person;
+		$location.path("/payment");
+	};
+}
+
+
+function PaymentCtrl($scope, personService) {
+
+	$scope.person = personService.person; 
 }
