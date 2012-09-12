27dbc6748efb01d6990b37f9959c3fff45dc811f
Phil <github@exclsr.com>
2012-09-11 21:32:08 -0700

    RSVP: Use emailjs for smtp email
    To send email, we set up an SMTP relay to
    Amazon SES (Simple Email Service). It's
    fairly basic and works fine.

--- rsvp/submit/index.js
+++ rsvp/submit/index.js
@@ -1,19 +1,43 @@
 var express = require('express');
-var app = express();
+var email   = require('emailjs');
 
+var app = express();
 app.use(express.bodyParser());
 
-app.get('/rsvp/submit/index.js', function (req, res) {
-    res.send(':-)');    
+var submitTarget = '/rsvp/submit/index.js';
+
+var smtpServer  = email.server.connect({
+	user:    "(put amazon smtp user here)", 
+	password:"(put amazon smtp password here)", 
+	host:    "email-smtp.us-east-1.amazonaws.com", 
+	ssl:     true
 });
 
-app.post('/rsvp/submit/index.js', function (req, res) {
+app.put(submitTarget, function (req, res) {
+	console.log(req.body);
+	res.send("Thanks!");
+
+	// send the message and get a callback with
+	// an error or details of the message that was sent
+	smtpServer.send({
+		text:    "Test email ... ", 
+		from:    "Corvallis Swing Dance Society <glenn@corvallisswing.com>", 
+		to:      "Guest <...>",
+		cc:      "<glenn@corvallisswing.com>",
+		subject: "testing emailjs"
+	}, 
+	function(err, message) { 
+		console.log(err || message); 
+	});
+});
+
+// For fun, do something with GET and POST.
+app.get(submitTarget, function (req, res) {
+	res.send(':-)');    
+});
+app.post(submitTarget, function (req, res) {
 	res.send("Please PUT your stuff.");	
 });
 
-app.put('/rsvp/submit/index.js', function (req, res) {
-	console.log(req.body);
-	res.send("Thanks!");	
-});
-
+// We get this PORT from iisnode
 app.listen(process.env.PORT);
