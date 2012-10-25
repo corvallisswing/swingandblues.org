de7832add24dc9914d04242a85c470c99d44eb42
Phil <github@exclsr.com>
2012-10-24 21:15:33 -0700

    Change "Swing and Blues" to "Blues and Swing"
    When the domain is bluesandswing.org, change things like
    "Corvallis Swing and Blues Weekend" to "Corvallis Blues and
    Swing Weekend," using jQuery.

--- js/isbluesfirst.js
+++ js/isbluesfirst.js
@@ -5,6 +5,18 @@ $(document).ready(function () {
 if (document.domain === "bluesandswing.org") {
 	$('head').append("<link rel='stylesheet' type='text/css' href='/css/blues.css' />")
 	$('#topnav').addClass('navbar-inverse');
+
+	$('title').html('Corvallis Blues and Swing Weekend');
+
+	var eventNames = $('.eventName');
+	eventNames.each(function(index, el) {		
+		$(this).html('Corvallis Blues and Swing Weekend');
+	});	
+
+	var fancyEventNames = $('.fancyEventName');
+	fancyEventNames.each(function(index, el) {		
+		$(this).html('Blues &amp; Swing Weekend');
+	});
 }
 
 });
