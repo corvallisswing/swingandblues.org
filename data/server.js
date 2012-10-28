994ea37e0bc708c3eb559cded686711ca6f32de7
Phil <github@exclsr.com>
2012-10-28 02:17:44 -0700

    Data: Start to use 'cradle'
    Start to use the cradle library for database access.
    This will have to remain partially complete until
    we get the admin section up and running. I apologize
    for the half-baked nature of this commit.

--- data/server.js
+++ data/server.js
@@ -4,16 +4,121 @@
 // Author: Phil
 // Created: September 2012
 
-var express  = require('express');
-var request  = require('request');
+var express = require('express');
+var request = require('request');
+var cradle  = require('cradle');
 
 var app = express();
 app.use(express.bodyParser());
 
 var db = function() {
+
+	var couchHost = 'http://localhost';
+	var couchPort = 5984;
+	var databaseName = 'weekendrsvp';
+
+	// Connect to Couch! 
+	var database = new(cradle.Connection)(couchHost, couchPort, {
+		cache: true,
+		raw: false
+	}).database(databaseName);
+
+	var createViews = function() {
+		// TODO: Add views to the database if they're
+		// not present. This is a secondary priority,
+		// as we can get by for now without this.
+		// (The views are at least saved below, and were
+		// already put in manually, for now.)
+	};
+
+	var createDatabaseAndViews = function() {
+		// Create database!
+		database.exists(function (err, exists) {
+			if (err) {
+				throw (err);
+			}
+			else if (exists) {
+				createViews();
+			}
+			else {
+				database.create();
+				createViews();
+			}
+		});
+	};
+
+	createDatabaseAndViews();
+
+	var isReady = false;
+
 	var localhostUrl = "http://localhost:5984";
+	var uuidUrl = localhostUrl + "/_uuids";
 	var databaseUrl = localhostUrl + "/weekendrsvp";
-	var getRolesUrl = databaseUrl + '/_design/rsvp/_view/roles?group=true';
+	var designUrl   = databaseUrl + '/_design/rsvp';
+	var getRolesUrl = designUrl + '/_view/roles?group=true';
+
+	//var couch = couchdb.srv(localhostUrl).db("weekendrsvp");
+
+	// Create database.
+	// request.get(databaseUrl, function (error, response, body) {
+	// 	if (error) {
+	// 		console.log("Error creating database. Do you have CouchDB installed?");
+	// 		return;
+	// 	}
+	// });
+
+	// 	// TODO: Don't assume that the database was created
+	// 	// successfully.
+
+	// 	// Create design document (with views)
+	// 	request.get(designUrl, function (error, response, body) {
+
+	// 		if (error) {
+	// 			// Booooooo.
+	// 			return;
+	// 		}
+
+	// 		var result = JSON.parse(body);
+	// 		// Create the design document if it doesn't exist.
+	// 		if (result.error && result.error === "not_found") {
+	// 			var data = {
+	// 				"_id": "_design/rsvp",
+	// 				language: "javascript",
+	// 				views: {
+	// 					roles: {
+	// 						map: function(doc) {
+	// 							if (doc.dancer && doc.dancer.role) {
+	// 									emit(doc.dancer.role, 1);
+	// 							}
+	// 						},
+	// 						reduce: function (keys, values, rereduce) {
+	// 							return sum(values);
+	// 						}
+	// 					}
+	// 				}
+	// 			};
+	// 			var designDocument = {
+	// 				uri : designUrl,
+	// 				json : data
+	// 			};
+
+	// 			request.put(designDocument, function (error, response, body) {
+	// 				if (error) { 
+	// 					// TODO: Communication error!
+	// 				}
+	// 				else if (body.ok) {
+	// 					// Stay cool.
+	// 					isReady = true;
+	// 				}
+	// 				else {
+	// 					// TODO: Freak out.
+	// 				}
+	// 			});
+	// 		}
+			
+	// 	});
+	// });
+
 
 	var getRoles = function (success, failure) {
 
