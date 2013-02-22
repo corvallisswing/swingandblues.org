aea481d0ef2be0120c76734bca5f444a400ccedc
Phil <github@exclsr.com>
2013-02-21 16:53:40 -0800

    Refactor: Move authentication into own file
    Move most of the authentication stuff into auth.js

--- data/server.js
+++ data/server.js
@@ -9,94 +9,22 @@ var request = require('request');
 var secrets = require('./lib/secrets.js');
 var db      = require('./lib/database.js').db;
 var emailer = require('./lib/emailer.js');
-
-var passport = require('passport')
-  , GoogleStrategy = require('passport-google').Strategy;
-//  , util = require('util')
-
+var auth    = require('./lib/auth.js');
 
 var sessionSecret = secrets.sessionSecret(); 
-var allowedUsers = secrets.allowedUsers();
 var serverPort = 3000;
-
 var loginFailureUrl = '/admin';
-var googleReturnUrl = '/data/admin/auth/google/return';
-
-var domain = 'whatever';
-var hostUrl = function() {
-	if (domain === 'localhost') {
-		return 'http://' + domain;// + ':' + serverPort;
-	}
-	else {
-		return 'http://' + domain;
-	}
-};
-
-var initPassport = function() {
-	// Use the GoogleStrategy within Passport.
-	//   Strategies in passport require a `validate` function, which accept
-	//   credentials (in this case, an OpenID identifier and profile), and invoke a
-	//   callback with a user object.
-	passport.use(new GoogleStrategy({
-		returnURL: hostUrl() + googleReturnUrl,
-		realm: hostUrl() + '/'
-	},
-	function(identifier, profile, done) {
-	    // asynchronous verification, for effect...
-	    process.nextTick(function () {
-
-	      // To keep the example simple, the user's Google profile is returned to
-	      // represent the logged-in user.  In a typical application, you would want
-	      // to associate the Google account with a user record in your database,
-	      // and return that user instead.
-	      profile.identifier = identifier;
-	      return done(null, profile);
-	  });
-	}
-	));
-};
-
-// middleware that does a few things the first
-// time it is called. we have this so that we
-// can initialize passportjs with our domain name.
-var isFirstRun = true;
-var firstRun = function(req, res, next) {
-	if (isFirstRun) {
-		domain = req.host;
-		initPassport();
-		isFirstRun = false;
-	}
-	return next();
-};
-
-// From the passport demo:
-//
-// Passport session setup.
-//   To support persistent login sessions, Passport needs to be able to
-//   serialize users into and deserialize users out of the session.  Typically,
-//   this will be as simple as storing the user ID when serializing, and finding
-//   the user by ID when deserializing.  However, since this example does not
-//   have a database of user records, the complete Google profile is serialized
-//   and deserialized.
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
-
-passport.deserializeUser(function(obj, done) {
-  done(null, obj);
-}); 
-
 
 var app = express();
 app.use(express.bodyParser());
-app.use(firstRun);
+app.use(auth.firstRun);
 // Required for passport:
 app.use(express.cookieParser());
 app.use(express.session({ secret: sessionSecret })); 
 // Initialize Passport!  Also use passport.session() middleware, to support
 // persistent login sessions (recommended).
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(auth.initialize());
+app.use(auth.session());
 
 // GET /data/admin/auth/google
 //   Use passport.authenticate() as route middleware to authenticate the
@@ -104,7 +32,7 @@ app.use(passport.session());
 //   the user to google.com.  After authenticating, Google will redirect the
 //   user back to this application at /data/admin/auth/google/return
 app.get('/data/admin/auth/google', 
-  passport.authenticate('google', { failureRedirect: loginFailureUrl }),
+  auth.authenticate('google', { failureRedirect: loginFailureUrl }),
    function(req, res) {
 	// This response doesn't matter, because we get redirected
 	// to /data/admin/auth/google/return anyway.
@@ -112,79 +40,18 @@ app.get('/data/admin/auth/google',
    });
 
 
-var authenticate = function(req, success, failure) {
-
-	return passport.authenticate('google', 
-		function (err, user, info) {
-
-			if (err) { 
-				failure(err);
-			}
-			else if (!user) { 
-				failure("Invalid login data");
-			}
-			else {
-				var primaryEmail = user.emails[0].value;
-				if (allowedUsers.indexOf(primaryEmail) >= 0) {
-					// req.login is added by the passport.initialize() middleware
-					// to manage login state. We need to call it directly, as we're
-					// overriding the default passport behavior.
-					req.login(user, function(err) {
-						if (err) { 
-							failure(err);
-						}
-						success();
-					});
-				}
-				else {
-					failure("Unknown email address");
-				}
-			}
-		}
-	);
-};
-
-// Authentication. This defines what we send
-// back to clients that want to authenticate
-// with the system.
-var authMiddleware = function(req, res, next) {
-
-	var success = function() {
-		// TODO: How does the client know whether it is authenticated?
-		res.redirect('http://' + req.host + '/admin');
-//		res.send(200, "Login successul");
-	};
-
-	var failure = function(error) {
-		console.log(error);
-		res.send(200, "Your email address isn't on the list of those who " + 
-			"have access. Make sure you're using the Google account you're " +
-			"intending to use, then ask Phil what's up (and give him the email " +
-			"address you're using to log in)."); 
-	};
-
-	// The auth library provides middleware that
-	// calls 'success' or 'failure' in the appropriate
-	// login situation.
-	var middleware = authenticate(req, success, failure);
-	middleware(req, res, next);
-};
-
-
 // GET /data/auth/google/return
 //   Use passport.authenticate() as route middleware to authenticate the
 //   request.  If authentication fails, the user will be redirected back to the
 //   login page.  Otherwise, the primary route function function will be called,
 //   which, in this example, will redirect the user to the home page.
-app.get(googleReturnUrl, authMiddleware);
+app.get(auth.googleReturnUrl, auth.authMiddleware);
 
 // Logout ...
 app.get('/data/admin/auth/logout', function(req, res){
   req.logout();
   res.redirect('/');
 });
-
-
 
 
 var roles = function(success, failure) {
