ff5bac9cc2d8c97c4657e59bd2b79fe4243d6e4e
Phil Manijak <github@exclsr.com>
2014-09-05 12:33:15 -0700

    Admin: Disable all access

--- routes/lib/auth.js
+++ routes/lib/auth.js
@@ -89,7 +89,7 @@ var authenticate = function(req, success, failure) {
 			else {
 				var primaryEmail = user.emails[0].value;
 				user.id = primaryEmail;
-				if (true) {
+				if (false) {
 				// TODO: if (allowedUsers.indexOf(primaryEmail) >= 0) {
 					// req.login is added by the passport.initialize() middleware
 					// to manage login state. We need to call it directly, as we're
