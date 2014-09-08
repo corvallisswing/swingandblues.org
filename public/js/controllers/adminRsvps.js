5e9b7274b7dac18bac20b777c140641748f827bd
Phil Manijak <github@exclsr.com>
2014-09-07 23:43:30 -0700

    Admin: Show rsvp details

--- public/js/controllers/adminRsvps.js
+++ public/js/controllers/adminRsvps.js
@@ -1,6 +1,35 @@
 'use strict'
 
 function AdminRsvps(rsvps, $scope) {
-    
+
+    var details = [];
+
+    $scope.getRsvp = function (id) {
+        var rsvp = undefined;
+        for (var i=0; i < rsvps.length; i++) {
+            if (rsvps[i]._id === id) {
+                rsvp = rsvps[i];
+                break;
+            }
+        }
+        return rsvp;
+    };
+
+    $scope.toggleDetails = function (id) {
+        var index = details.indexOf(id);
+        if (index < 0) {
+            details.push(id);
+        }
+        else {
+            details.splice(index, 1);
+        }
+    };
+
+    $scope.isShowing = function (id) {
+        if (details.indexOf(id) < 0) {
+            return false;
+        }
+        return true;
+    };
 }
 AdminRsvps.$inject = ['rsvps', '$scope'];
