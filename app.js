828ec480ecd8954ea1f9d7d17a6eab63dfe7dae9
Phil Manijak <github@exclsr.com>
2014-09-10 14:37:58 -0700

    Payments: Process instant Paypal notifications

--- app.js
+++ app.js
@@ -56,22 +56,32 @@ var handleErrors = function () {
     // will print stacktrace
     if (app.get('env') === 'development') {
         app.use(function(err, req, res, next) {
-            res.status(err.status || 500);
-            res.render('error', {
-                message: err.message,
-                error: err
-            });
+            if (res.headerSent) {
+                errors.log(err);
+            }
+            else {
+                res.status(err.status || 500);
+                res.render('error', {
+                    message: err.message,
+                    error: err
+                });    
+            }
         });
     }
 
     // production error handler
     // no stacktraces leaked to user
     app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: {}
-        });
+        if (res.headerSent) {
+            errors.log(err);
+        }
+        else {
+            res.status(err.status || 500);
+            res.render('error', {
+                message: err.message,
+                error: {}
+            });    
+        }
     });
 };
 
--- routes/payments.js
+++ routes/payments.js
@@ -1,16 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+var superagent = require('superagent');
+
 var db = require('./lib/data/payments');
+var errors = require('./lib/errors');
 
-// This URL is specified in our Paypal account. It receives
-// a JSON object for every payment to our account, not just
-// weekend payments.
-router.post('/paypal/', function (req, res) {    
-    console.log("PAYPAL NOTIFICATION:");
-    console.log(req.body);
+var verifyIpn = function (ipn, callback) {
+    if (!ipn) {
+        return callback();
+    }
 
-    var paypal = req.body;
+    var req = superagent.post('https://www.sandbox.paypal.com/cgi-bin/webscr');
+    // Reference: 
+    // https://developer.paypal.com/webapps/developer/docs/classic/ipn/integration-guide/IPNIntro/#id08CKFJ00JYK
+    req.send('cmd=_notify-validate');
+
+    for (var propName in ipn) {
+        req.send(propName + '=' + ipn[propName]);
+    }
+
+    req.end(function (res) {
+        callback(null, res.text);
+    });
+};
+
+var savePayment = function (paypal, callback) {
     // AQUACQJZJ5CDQ: Weekend pass
     // G8GANEEDRWNUY: Weekend pass and shirt
     var weekendItemNumbers = ['AQUACQJZJ5CDQ', 'G8GANEEDRWNUY'];
@@ -35,7 +50,7 @@ router.post('/paypal/', function (req, res) {
         isValidItemNumber(paypal.item_number);
 
     if (!shouldProcessNotification) {
-        return res.status(200).send();
+        return callback();
     }
 
     var email = paypal.option_selection1;
@@ -47,17 +62,55 @@ router.post('/paypal/', function (req, res) {
         method: "paypal",
         isWeekendPass: isWeekendOrder,
         isShirt: isShirt,
-        gross: paypal.payment_gross,
-        fee: paypal.payment_fee
+        gross: paypal.mc_gross,
+        fee: paypal.mc_fee,
+        timestamp: Date.now(),
+        meta: {
+            date: paypal.payment_date,
+            firstName: paypal.first_name,
+            lastName: paypal.last_name,
+            transactionId: paypal.txn_id,
+            receiverEmail: paypal.receiver_email,
+            currency: paypal.mc_currency    
+        }
     };
 
-    // TODO: Send POST back to Paypal
-    // See: https://developer.paypal.com/webapps/developer/docs/classic/ipn/integration-guide/IPNIntro/
-    db.add(payment, function (err) {
+    // TODO: If we're concerned about fraud, we'll want to 
+    // double-check the mc_gross and the receiver_email
+    // is ours (otherwise someone could POST us a payment to
+    // another paypal account).
+    db.add(payment, callback);
+};
+
+// This URL is specified in our Paypal account. It receives
+// a JSON object for every payment to our account, not just
+// weekend payments.
+router.post('/paypal/', function (req, res) {
+    console.log("PAYPAL NOTIFICATION:");
+    console.log(req.body);
+
+    // As per the Paypal spec, respond with OK right away.
+    res.status(200).send();
+
+    // Then send a POST, containing the request.
+    verifyIpn(req.body, function (err, response) {
         if (err) {
             errors.log(err);
+            return;
         }
-        res.status(200).send();  
+
+        if (response !== "VERIFIED") {
+            console.log("PAYPAL PAYMENT IGNORED.");
+            return;
+        }
+
+        // Save the payment to the database.
+        savePayment(req.body, function (err) {
+            if (err) {
+                errors.log(err);
+            }
+            console.log("PAYPAL PAYMENT SAVED.");
+        });
     });
 });
 
