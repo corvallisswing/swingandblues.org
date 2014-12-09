9f1c2c7e3132e757ad42076b2f51dc98a3c091bb
Phil Manijak <github@exclsr.com>
2014-12-09 13:19:58 -0800

    Admin: Housing: Only show appropriate details

--- public/js/controllers/adminRsvps.js
+++ public/js/controllers/adminRsvps.js
@@ -4,13 +4,17 @@ function AdminRsvps(rsvps, $scope) {
 
     var details = [];
 
-    $scope.getRsvp = function (id) {
+    $scope.getRsvp = function (id, section) {
         var rsvp = undefined;
         for (var i=0; i < rsvps.length; i++) {
             if (rsvps[i]._id === id) {
                 rsvp = rsvps[i];
                 break;
             }
+        }
+
+        if (section) {
+            return rsvp[section];
         }
         return rsvp;
     };
