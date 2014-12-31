50bf014757e92f2ebaea7250a9ce01c879ec23e6
Phil Manijak <github@exclsr.com>
2014-12-31 15:20:42 -0800

    Add press page, 4x6 card image
    Sometimes it's easier to use the internet for 
    transferring files instead of mailing cards and
    whatnot.

--- routes/index.js
+++ routes/index.js
@@ -4,27 +4,31 @@ var router = express.Router();
 var errors = require('./lib/errors');
 var emailer = require('./lib/email/contactEmailer');
 
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
     res.render('index', { title: 'Corvallis Swing and Blues Weekend' });
 });
 
-router.get('/about', function(req, res) {
+router.get('/about', function (req, res) {
     res.redirect('/');
 });
 
-router.get('/schedule', function(req, res) {
+router.get('/schedule', function (req, res) {
     res.render('schedule', { title: 'Schedule: Corvallis Swing and Blues Weekend' });
 });
 
-router.get('/contact', function(req, res) {
+router.get('/contact', function (req, res) {
     res.render('contact', { title: 'Contact Corvallis Swing and Blues Weekend' });
 });
 
-router.post('/contact', function(req, res) {
+router.post('/contact', function (req, res) {
     var envelope = req.body;
     emailer.sendEmail(envelope, errors.guard(res, function () {
         res.status(200).send();
     }));
+});
+
+router.get('/press', function (req, res) {
+	res.render('press', { title: 'Press resources for Corvallis Swing and Blues Weekend' });
 });
 
 module.exports = router;
